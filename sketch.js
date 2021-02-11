60c654e4aef00fe780579887b689fbe1cad2a20f
Radhika2008 <74184326+Radhika2008@users.noreply.github.com>
2021-02-11 20:44:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+var hypnoticBall,database,position;
+
+
+function setup(){
+     database=firebase.database();
+     console.log(database);
+
+
+
+
+    createCanvas(500,500);
+    hypnoticBall = createSprite(250,250,10,10);
+    hypnoticBall.shapeColor = "red";
+    var  hypnoticBallPosition=database.ref('ball/position');
+    hypnoticBallPosition.on("value",readPosition,showError);
+
+
+
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+
+
+function readPosition(data){
+    position=data.val();
+    hypnoticBall.x=postion.x;
+    hypnoticBall.y=postion.y;
+}
+ function writePosition(x,y){
+    database.ref('ball/position').set({
+     'x': position.x+x,'y': position.y+y
+    })
+ }
+function showError(){
+    console.log("Error in writing to the database");
+}
